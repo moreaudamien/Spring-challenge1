e19556dacd0f7528df173137316f1da149a59458
moreaudamien <moreau_student2023@wilder.school>
2023-07-17 10:58:04 +0200

    first commit

---
+++ src/main/java/com/wildcodeschool/challange1/Challange1Application.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.challange1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Challange1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Challange1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/wildcodeschool/challange1/Challange1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.challange1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Challange1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
